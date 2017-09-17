f2e31527b7b0ef431d1ea7713a528075c2a0bfea
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-09-17 21:21:50 +0900

    Refactor to WordLists

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import AppIntro from './AppIntro';
 import AppHeader from './AppHeader';
-
-import './App.css';
+import WordLists from './Containers/WordLists';
 
 class App extends Component {
   render() {
@@ -20,78 +19,8 @@ class App extends Component {
         </div>
 
         <div className="ui divider hidden"></div>
+        <WordLists />
 
-        <div className="ui container">
-
-          <div className="ui large header">Words List</div>
-
-          <div className="ui divider hidden"></div>
-
-          <div className="ui cards">
-            <div className="card">
-              <div className="content">
-                <div className="header">TOEIC Words</div>
-                <div className="description">
-                  1423 words
-                </div>
-              </div>
-              <div className="ui bottom attached button">
-                <i className="add icon"></i>
-                View Word List
-              </div>
-            </div>
-            <div className="card">
-              <div className="content">
-                <div className="header">TOEFL Words</div>
-                <div className="description">
-                  852 words
-                </div>
-              </div>
-              <div className="ui bottom attached button">
-                <i className="add icon"></i>
-                View Word List
-              </div>
-            </div>
-            <div className="card">
-              <div className="content">
-                <div className="header">GRE Words</div>
-                <div className="description">
-                  5342 words
-                </div>
-              </div>
-              <div className="ui bottom attached button">
-                <i className="add icon"></i>
-                View Word List
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="content">
-                <div className="header">GMAT Words</div>
-                <div className="description">
-                  342 words
-                </div>
-              </div>
-              <div className="ui bottom attached button">
-                <i className="add icon"></i>
-                View Word List
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="content">
-                <div className="header">Create New List</div>
-                <div className="description">
-                </div>
-              </div>
-              <div className="ui bottom attached button">
-                <i className="add icon"></i>
-                Make New List
-              </div>
-            </div>
-
-          </div>
-        </div>
       </div>
     );
   }
---
+++ src/Containers/WordLists.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+
+export default class WordLists extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+        <div className="ui large header">Words List</div>
+        <div className="ui divider hidden"></div>
+
+        <div className="ui cards">
+          <div className="card">
+            <div className="content">
+              <div className="header">TOEIC Words</div>
+              <div className="description">
+                1423 words
+              </div>
+            </div>
+            <div className="ui bottom attached button">
+              <i className="add icon"></i>
+              View Word List
+            </div>
+          </div>
+          <div className="card">
+            <div className="content">
+              <div className="header">TOEFL Words</div>
+              <div className="description">
+                852 words
+              </div>
+            </div>
+            <div className="ui bottom attached button">
+              <i className="add icon"></i>
+              View Word List
+            </div>
+          </div>
+          <div className="card">
+            <div className="content">
+              <div className="header">GRE Words</div>
+              <div className="description">
+                5342 words
+              </div>
+            </div>
+            <div className="ui bottom attached button">
+              <i className="add icon"></i>
+              View Word List
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="content">
+              <div className="header">GMAT Words</div>
+              <div className="description">
+                342 words
+              </div>
+            </div>
+            <div className="ui bottom attached button">
+              <i className="add icon"></i>
+              View Word List
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="content">
+              <div className="header">Create New List</div>
+              <div className="description">
+              </div>
+            </div>
+            <div className="ui bottom attached button">
+              <i className="add icon"></i>
+              Make New List
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
